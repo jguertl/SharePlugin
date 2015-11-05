2e44134ba98f3523478ab47bc10cd644abc11287
James Montemagno <james.montemagno@gmail.com>
2015-11-04 17:37:00 -0800

    Added xml comments

--- Share/Share.Plugin.Abstractions/IShare.cs
+++ Share/Share.Plugin.Abstractions/IShare.cs
@@ -8,8 +8,28 @@ namespace Share.Plugin.Abstractions
     /// </summary>
     public interface IShare
     {
+        /// <summary>
+        /// Simply share text on compatible services
+        /// </summary>
+        /// <param name="text">Text to share</param>
+        /// <param name="title">Title of popup on share (not included in message)</param>
+        /// <returns>awaitable Task</returns>
         Task Share(string text, string title = null);
+        
+        /// <summary>
+        /// Open a browser to a specific url
+        /// </summary>
+        /// <param name="url">Url to open</param>
+        /// <returns>awaitable Task</returns>
         Task OpenBrowser(string url);
+
+        /// <summary>
+        /// Share a link url with compatible services
+        /// </summary>
+        /// <param name="url">Link to share</param>
+        /// <param name="message">Message to share</param>
+        /// <param name="title">Title of the popup</param>
+        /// <returns>awaitable Task</returns>
         Task ShareLink(string url, string message = null, string title = null);
 
     }
