cae925d0f335236c4722c21e912204c2886f0591
Dominik Weber <d.weber@openasapp.net>
2016-09-22 00:47:44 +0200

    Add ShareColor constructors

--- Share/Share.Plugin.Abstractions/ShareColor.cs
+++ Share/Share.Plugin.Abstractions/ShareColor.cs
@@ -27,5 +27,37 @@ namespace Plugin.Share.Abstractions
         /// Blue 0-255
         /// </summary>
         public int B { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShareColor"/> class with default values.
+        /// </summary>
+        public ShareColor()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShareColor"/> class with the specified values. Alpha is set to 255 (fully opaque).
+        /// </summary>
+        /// <param name="r">Red 0-255</param>
+        /// <param name="g">Green 0-255</param>
+        /// <param name="b">Blue 0-255</param>
+        public ShareColor(int r, int g, int b) : this(r, g, b, 255)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShareColor"/> class with the specified values.
+        /// </summary>
+        /// <param name="r">Red 0-255</param>
+        /// <param name="g">Green 0-255</param>
+        /// <param name="b">Blue 0-255</param>
+        /// <param name="a">Alpha 0-255</param>
+        public ShareColor(int r, int g, int b, int a)
+        {
+            A = a;
+            R = r;
+            G = g;
+            B = b;
+        }
     }
 }
