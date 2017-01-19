18e72f7cb3ffd9706fbd6e8882f7a2db05d3c2f6
James Montemagno <james.montemagno@gmail.com>
2017-01-19 17:07:49 -0600

    Update bootstrapper.ps1

--- bootstrapper.ps1
+++ bootstrapper.ps1
@@ -63,7 +63,7 @@ if (!(Test-Path $XC_EXE)) {
 # Restore tools from NuGet.
 Push-Location
 Set-Location $TOOLS_DIR
-Invoke-Expression "$NUGET3_EXE install -ExcludeVersion -Source https://www.nuget.org/api/v3"
+Invoke-Expression "$NUGET3_EXE install -ExcludeVersion -Source https://www.nuget.org/api/v2"
 Pop-Location
 if ($LASTEXITCODE -ne 0)
 {
